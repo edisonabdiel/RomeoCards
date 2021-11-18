cebb65c4a54485682ed8359b6e32b6d30471293f
Edison Abdiel <edison.abdiel@soundcloud.com>
2021-11-18 21:59:49 +0100

    Initial Commit & Basic Structure

---
+++ components/index.js
@@ -0,0 +1,4 @@
+export { default as Footer } from './Footer';
+export { default as Navbar } from './Navbar';
+export { default as SearchBar } from './SearchBar';
+export { default as UserCard } from './UserCard';
